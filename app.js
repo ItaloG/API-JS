f92983a1ba5029cd505696958435cf66183bb2d5
Italo Gabriel <italog03@gmail.com>
2021-06-08 11:53:59 -0300

    Inplentação dos dados nos inputs

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 
 // Fazer o cosumo da API de cep para aparecer automaticamente os dado de endereço.
 
+const  showData = (data) => {
+    const endereco = document.getElementById('endereco');
+    const bairro = document.getElementById('bairro');
+    const cidade = document.getElementById('cidade');
+    const estado = document.getElementById('estado');
+    
+    console.log(data);
+    endereco.value = data.logradouro;
+    bairro.value = data.bairro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+};
 
 /**
  *  Recebe um evento, faz uma requisição à API e retorna os dados da requisição
